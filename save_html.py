d8cfbb848b70395e90dc42ed860f5441871040f3
_ <thomash1996@live.com>
2023-08-21 00:35:39 -0500

    updated save html

--- save_html.py
+++ save_html.py
@@ -9,18 +9,18 @@ def response(flow: http.HTTPFlow) -> None:
     if "image/jpeg" or "image/png" in flow.response.headers.get("content-type", ""):
         # Save the HTML content to a file with a unique name
         cur_time = str(time.time())
-        filename = f"html_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.png"
+        filename = f"image_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.png"
         with open(filename, "wb") as f:
             f.write(flow.response.content)
-        print(f"Saved HTML content to {filename}")
+        print(f"Saved Image content to {filename}")
 
 
     elif "text/html" in flow.response.headers.get("content-type", ""):
         # Save the HTML content to a file with a unique name
         cur_time = str(time.time())
         filename = f"html_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.html"
-        #with open(filename, "wb") as f:
-        with open(filename, "a") as f:
+        with open(filename, "wb") as f:
+        #with open(filename, "a") as f:
             f.write(flow.response.content)
         print(f"Saved HTML content to {filename}")
 
