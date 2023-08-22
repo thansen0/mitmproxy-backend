788430c863df93cc3d8676a1314e673f1b823927
_ <thomash1996@live.com>
2023-08-22 00:11:23 -0500

    can modify image upon request

---
+++ modify_response.py
@@ -0,0 +1,42 @@
+from mitmproxy import ctx, http
+import time
+import numpy as np
+from CustomList import CustomList
+import pdb
+import cv2
+import logging
+
+class ModifyResponse:
+    def __init__(self):
+        self.num = 0
+
+        # Create watch list
+        self.watch_list = CustomList("mixed_use.txt")
+
+        # print(self.watch_list)
+
+    def response(self, flow: http.HTTPFlow) -> None:
+
+        cur_time = str(time.time())
+
+        if "image/jpeg" in flow.response.headers.get("content-type", "") or "image/png" in flow.response.headers.get("content-type", ""):
+            logging.info("image/jpeg Response headers: %s" % str(flow.response.headers.get("content-type")))
+            # Save the HTML content to a file with a unique name
+            filename = f"image_original_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.png"
+            with open(filename, "wb") as f:
+                f.write(flow.response.content)
+            # print(f"Saved Image content to {filename}")
+
+            fill_image_path = "./fill.jpeg"
+            #img = cv2.imread(fill_image_path)
+            with open(fill_image_path, "rb") as file:
+                file_bytes = file.read()
+            flow.response.content = file_bytes # cv2.imencode('jpeg', img)
+
+            
+
+
+
+
+addons = [ModifyResponse()]
+
