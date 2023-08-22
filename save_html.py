cb94403fe3c3eb0af896eb208d4eccd6ba3812cf
_ <thomash1996@live.com>
2023-08-21 22:22:44 -0500

    save html now works properly

--- save_html.py
+++ save_html.py
@@ -1,14 +1,27 @@
-# save_html.py
-
 from mitmproxy import http
 import time
+import logging
+import pdb
 
 def response(flow: http.HTTPFlow) -> None:
     # Check if the response contains HTML content
 
-    if "image/jpeg" or "image/png" in flow.response.headers.get("content-type", ""):
+    cur_time = str(time.time())
+
+    if "text/css" in flow.response.headers.get("content-type", ""):
+        logging.info("text/css Response headers, no save: %s" % str(flow.response.headers.get("content-type")))
+
         # Save the HTML content to a file with a unique name
-        cur_time = str(time.time())
+#        filename = f"image_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.png"
+#        with open(filename, "wb") as f:
+#            f.write(flow.response.content)
+#        print(f"Saved Image content to {filename}")
+
+
+    #elif ("image/jpeg" or "image/png") in flow.response.headers.get("content-type", ""):
+    elif "image/jpeg" in flow.response.headers.get("content-type", "") or "image/png" in flow.response.headers.get("content-type", ""):
+        logging.info("image/jpeg Response headers: %s" % str(flow.response.headers.get("content-type", "")))
+        # Save the HTML content to a file with a unique name
         filename = f"image_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.png"
         with open(filename, "wb") as f:
             f.write(flow.response.content)
@@ -16,12 +29,22 @@ def response(flow: http.HTTPFlow) -> None:
 
 
     elif "text/html" in flow.response.headers.get("content-type", ""):
+        logging.info("text/html Response headers: %s" % str(flow.response.headers.get("content-type", "")))
         # Save the HTML content to a file with a unique name
-        cur_time = str(time.time())
-        filename = f"html_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.html"
-        with open(filename, "wb") as f:
-        #with open(filename, "a") as f:
+        tfilename = f"text_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.html"
+        with open(tfilename, "wb") as f:
             f.write(flow.response.content)
-        print(f"Saved HTML content to {filename}")
+        print(f"Saved HTML content to {tfilename}")
+
+    elif "application/json" in flow.response.headers.get("content-type", ""):
+        logging.info("text/html Response headers: %s" % str(flow.response.headers.get("content-type", "")))
+        # Save the HTML content to a file with a unique name
+        filename = f"json_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}.html"
+        with open(filename, "wb") as f:
+            f.write(flow.response.content)
+        print(f"Saved JSON content to {filename}")
+
+    else:
+        logging.info("else response headers: %s" % str(flow.response))
 
 # Usage: mitmdump -s save_html.py
