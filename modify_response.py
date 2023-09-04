c5ad8a4b9912b991b446cf59a62b59d0333c86d1
Thomas Hansen <thomash1996@live.com>
2023-09-04 10:33:44 -0500

    added redis lookup

--- modify_response.py
+++ modify_response.py
@@ -2,10 +2,12 @@ from mitmproxy import ctx, http
 import numpy as np
 from CustomList import CustomList
 from nsfw_detector import predict
+import redis
 import logging
 import time
 import json
 import pdb
+import re
 import os
 
 class ModifyResponse:
@@ -16,23 +18,70 @@ class ModifyResponse:
         self.watch_list = CustomList("mixed_use.txt")
 
 
+        # Create a Redis connection
+        self.redis_host = 'localhost'  # Replace with your Redis server's hostname or IP address
+        self.redis_port = 6379         # Replace with your Redis server's port
+        self.redis_db = 0              # Replace with your desired Redis database number (default is 0)
+
+        # Connect to Redis
+        try:
+            self.ri = redis.StrictRedis(host=self.redis_host, port=self.redis_port, db=self.redis_db, decode_responses=True)
+            print("Connected to Redis")
+        except Exception as e:
+            print(f"Error connecting to Redis: {e}")
+            exit(1)
+
+    def _url_exists(self, url):
+        # perform modifications to url if needed
+        # Define a regex pattern to extract the domain and subreddit
+
+
+        print("The URL passed in:"+url)
+        if "reddit.com" in url:
+            logging.info("This is a reddit URL")
+            pattern = r'https?://(www\.)?reddit\.com/r/([^/]+)/'
+    
+            match = re.search(pattern, url)            
+            if match:
+                # Extract the domain and subreddit from the match object
+                domain = match.group(0)  # Full matched URL
+                subreddit = match.group(2)  # Subreddit part
+
+                # url_key = f'reddit.com/r/{subreddit}/'.lower()
+                url_key = subreddit.lower()
+
+                return self.ri.exists(url_key)
+
+            logging.info("Reddit in URL but match failed")
+            return False
+
+        logging.info("Reddit is not in URL")
+
+        return False
+
     def request(self, flow: http.HTTPFlow) -> None:
         #breakpoint()
-        host = flow.request.headers[b'host']
+        host = flow.request.headers[b'host'] # i.e. reddit.com
         #full_url = flow.request.headers[b'Referer']
         #print("full url: ", full_url)
+        logging.info(flow.request)
+        # print("host: ", host)
 
-        if "thomashansen.xyz" in host:
-            if "/blog/" in str(flow.request):
-                flow.kill()
-                logging.info("Killed %s flow" % flow.request)
+        request_str = str(flow.request) # i.e. GET reddit.com:80/full/url
+        logging.info(request_str)
+
+        if self._url_exists(request_str):
+            flow.kill()
+            logging.info("Killed %s flow" % flow.request)
+
 
 
     def response(self, flow: http.HTTPFlow) -> None:
         cur_time = str(time.time())
 
-        if "image/jpeg" in flow.response.headers.get("content-type", "") or "image/png" in flow.response.headers.get("content-type", ""):
-            logging.info("image/jpeg Response headers: %s" % str(flow.response.headers.get("content-type")))
+        #if "image/jpeg" in flow.response.headers.get("content-type", "") or "image/png" in flow.response.headers.get("content-type", ""):
+        if "image" in flow.response.headers.get("content-type", ""):
+            #logging.info("image/jpeg Response headers: %s" % str(flow.response.headers.get("content-type")))
             # Save the HTML content to a file with a unique name
             filename = f"image_original_{cur_time}_{flow.request.host}_path_{flow.request.path.replace('/', '_')}"
             with open(filename, "wb") as f:
@@ -40,7 +89,7 @@ class ModifyResponse:
                 #breakpoint()
 
                 classification = predict.classify(self.nsfw_model, filename)
-                logging.info(classification)
+                # logging.info(classification)
 
                 # may not be filename, check log
                 neutral_perc = classification[filename]['neutral'] # not porn if near 1
@@ -48,8 +97,8 @@ class ModifyResponse:
                 sexy_perc = classification[filename]['sexy'] # sexy if near 1
                 hentai_perc = classification[filename]['hentai'] # hentai if near 1
 
-                if neutral_perc < 0.7 or porn_perc > 0.7 or hentai_perc > 0.7:
-                    logging.info("neutral %d, porn %d, hentai %d", neutral_perc, porn_perc, hentai_perc)
+                if neutral_perc < 0.5 or porn_perc > 0.5 or hentai_perc > 0.5:
+                    logging.info("filling; neutral %d, porn %d, hentai %d", neutral_perc, porn_perc, hentai_perc)
                     # NSFW image, replace with fill
                     fill_image_path = "./fill.jpeg"
                     with open(fill_image_path, "rb") as file:
@@ -59,12 +108,6 @@ class ModifyResponse:
                 # delete image
                 os.remove(filename)
 
-
-#        elif "application/json" in flow.response.headers.get("content-type", ""):
-#            if flow.response.status_code >= 200 or flow.response.status_code < 300:
-#                if "reddit.com" in flow.response.headers.get('host', ''):
-#                    raw_json = flow.response.content
-#                    input_json = json.loads(raw_json)
 
 addons = [ModifyResponse()]
 
