5d97d7b5954e9ffc7e2e5946760fddcaa3f71d53
_ <thomash1996@live.com>
2023-11-05 13:20:19 -0600

    testing with rodauth now

--- userapp/signin-devise.py
+++ userapp/signin-devise.py
@@ -1,20 +1,20 @@
 import requests
 
 #url = 'https://your-rails-app.com/authenticate'  # Replace with your authentication endpoint URL
-# url = 'http://127.0.0.1:3000/users/sign_in'
-url = 'http://127.0.0.1:3000/api/v1/sign_in.json'
+#url = 'http://127.0.0.1:3000/api/auth/login'
+url = 'http://127.0.0.1:3000/login'
 # url = "http://localhost:3000/api/v1/sign_in"
-data = {
-    'user': {
-        'email': 'fake@email.com',
-        'password': 'password'
-    }
-}
-
 #data = {
-#    'email': 'fake@email.com',
-#    'password': 'password'
+#    'user': {
+#        'email': 'fake@email.com',
+#        'password': 'password'
+#    }
 #}
+
+data = {
+    'email': 'fake@email.com',
+    'password': 'password'
+}
 
 response = requests.post(url, json=data)
 #response = requests.get(url, auth=('fake@email.com', 'password'))
@@ -26,6 +26,7 @@ if response.status_code == 200:
     # Store the token for future requests
     #print(auth_token)
     print("Successful login")
+    print(response.json())
 else:
     # Authentication failed
     print('Authentication failed. Check credentials or handle the error.')
