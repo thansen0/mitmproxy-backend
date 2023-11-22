3c611d6a67bc56cbef8793ae571134edd5edd326
Thomas Hansen <thomash1996@live.com>
2023-11-22 05:45:26 +0000

    changed to match new proto file

--- server-code/RecieveConnections.py
+++ server-code/RecieveConnections.py
@@ -14,10 +14,11 @@ class CreateConnectionServicer(connection_pb2_grpc.CreateConnectionServicer):
     def StartConnection(self, request, context):
         # Your server logic here
         response = connection_pb2.ConnectionResp(
-            username="parent",
+            email="parent@email.com",
             serverPubKey="ServerPublicKey",
             portNumber=5000,
-            serverIPAddr="45.76.232.143"
+            serverIPAddr="45.76.232.143",
+            certificateFileCrt="cert file crt"
         )
         print("returning response: ", response)
         return response
@@ -26,6 +27,7 @@ def run_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     connection_pb2_grpc.add_CreateConnectionServicer_to_server(CreateConnectionServicer(), server)
     server.add_insecure_port("[::]:50051")
+    print("starting server")
     server.start()
     server.wait_for_termination()
 
