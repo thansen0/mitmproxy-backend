5b1f268fcb1b47481875311750fcd828557024f6
_ <thomash1996@live.com>
2023-11-29 03:01:23 -0600

    added new python-based mitmproxy wireguard startup method

---
+++ server-code/wireguard_server_impl.py
@@ -0,0 +1,107 @@
+import mitmproxy_wireguard
+import asyncio
+import logging
+import time
+import configparser
+import subprocess
+from modify_response import ModifyResponse
+
+from mitmproxy.options import Options
+#from mitmproxy.proxy.config import ProxyConfig
+from mitmproxy.tools.dump import DumpMaster
+from mitmproxy.tools.main import mitmdump
+
+logging.Formatter.convert = time.gmtime
+logger = logging.getLogger()
+
+public_ip_addr = "45.76.232.143" # ip you would ping to reach host server
+
+async def main():
+    # import configuration files information
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    client_pubkey = config['CLIENT']['pub_key']
+    email = config['CLIENT']['email']
+    server_privkey = config['SERVER']['priv_key']
+    server_pubkey = config['SERVER']['pub_key']
+
+    wg_server = await mitmproxy_wireguard.start_server(
+        "0.0.0.0",
+        51820,
+        server_privkey,
+        [client_pubkey],
+        handle_connection,
+        receive_datagram,
+    )
+
+    options = Options()
+    master = DumpMaster(options)
+    master.addons.add(ModifyResponse()) # may have to remove addon in class??
+    master.server = wg_server
+
+
+    try:
+        print("starting server")
+        # starting server
+        await master.run()
+    except:
+        print("bad news bears")
+        print(m)
+        os.exit(1)
+
+    ########################
+#    wg0conf_path = "/wg0.conf"
+#
+#    conf_file_contents = f"""
+#[Interface]
+#Address = 10.0.0.1/32
+#SaveConfig = true
+#PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE; iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080; iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j REDIRECT --to-port 8080
+#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o enp1s0 -j MASQUERADE; iptables -t nat -D PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080; iptables -t nat -D PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j REDIRECT --to-port 8080
+#ListenPort = 51820
+#PrivateKey = {server_privkey}
+#
+#[Peer]
+#PublicKey = {client_pubkey}
+#AllowedIPs = 10.0.0.2/24
+#    """
+
+    # create wgo.conf file
+#    with open(wg0conf_path, "w") as conf_file:
+#        conf_file.write(conf_file_contents)
+
+    #print("starting server")
+    #subprocess.run(["mitmdump", "-s", "modify_response.py", "--mode", "wireguard", "--certs", "/etc/keys/cert.pem"], shell=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+
+    ########################
+    def stop(*_):
+        wg_server.close()
+
+
+    # Below also runs, unsure which is better
+    #print("waiting on server to close")
+    #await master.server.wait_closed()
+
+
+
+async def handle_connection(rw: mitmproxy_wireguard.TcpStream):
+    while True:
+        data = await rw.read(4096)
+
+        # check if the connection was closed
+        if len(data) == 0:
+            break
+
+        rw.write(data)
+        await rw.drain()
+
+    rw.close()
+
+def receive_datagram(_data, _src_addr, _dst_addr):
+    pass
+
+if __name__ == '__main__':
+    #loop = asyncio.get_event_loop()
+    #loop.run_until_complete(main())
+    asyncio.run(main(), debug=True)
