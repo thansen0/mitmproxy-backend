a64c042f364ba77536ff2fe098dd873f0934f4b9
Thomas Hansen <thomash1996@live.com>
2023-12-03 07:15:22 +0000

    cleaned up comments

--- server-code/terminal_run_wireguard.py
+++ server-code/terminal_run_wireguard.py
@@ -21,15 +21,6 @@ async def main():
     server_privkey = config['SERVER']['priv_key']
     server_pubkey = config['SERVER']['pub_key']
 
-    #wg_server = await mitmproxy_wireguard.start_server(
-    #    "0.0.0.0",
-    #    51820,
-    #    server_privkey,
-    #    [client_pubkey],
-    #    handle_connection,
-    #    receive_datagram,
-    #)
-
     ########################
     # construct wireguard.conf key file
     conf_path = Path("wireguard.conf")
@@ -46,30 +37,8 @@ async def main():
     conf_file_path = str(conf_path.absolute())
 
 
-
-#    wg0conf_path = "wg0.conf"
-#
-#    conf_file_contents = f"""
-#[Interface]
-#Address = 10.0.0.1/32
-#SaveConfig = true
-#PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE; iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080; iptables -t nat -A PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j REDIRECT --to-port 8080
-#PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o enp1s0 -j MASQUERADE; iptables -t nat -D PREROUTING -i wg0 -p tcp -m tcp --dport 80 -j REDIRECT --to-port 8080; iptables -t nat -D PREROUTING -i wg0 -p tcp -m tcp --dport 443 -j REDIRECT --to-port 8080
-#ListenPort = 51820
-#PrivateKey = {server_privkey}
-#
-#[Peer]
-#PublicKey = {client_pubkey}
-#AllowedIPs = 10.0.0.2/24
-#    """
-
-    # create wgo.conf file
-#    with open(wg0conf_path, "w") as conf_file:
-#        conf_file.write(conf_file_contents)
-
     logger.info(f"starting server with wireguard keys at {conf_file_path}")
     subprocess.run(["mitmdump", "-s", "modify_response.py", "--mode", f"wireguard:{conf_file_path}@51820"]) # , stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    #subprocess.run(["mitmdump", "-s", "modify_response.py", "--mode", "wireguard", "--certs", "/etc/keys/cert.pem"], shell=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
     ########################
