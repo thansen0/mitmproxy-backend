9148beb7efa229b56f3366b6c00db5afe0945603
_ <thomash1996@live.com>
2023-12-19 01:17:28 -0600

    running example of POW api authentication

---
+++ userapp/api-getCF.py
@@ -0,0 +1,62 @@
+import requests
+import json
+from io import StringIO
+
+def authenticate_user(email, password):
+    url = "http://localhost:4000/api/v1/session"
+    # url = "http://localhost:4000/api/api_devices"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "user": {
+            "email": email,
+            "password": password
+        }
+    }
+
+    print(url)
+    response = requests.post(url, json=data, headers=headers)
+    return response #response.json()
+
+
+def show_device(deviceId, token):
+    headers = {'Authorization': f'{token}'}
+
+    api_url = f'http://localhost:4000/api/v1/getContentFilters'  # Replace with your actual API endpoint
+    print(api_url)    
+
+    response = requests.get(api_url)
+
+    if response.status_code >= 200 and response.status_code < 300:
+        return response
+    else:
+        print(f"Error: {response.status_code}, Message: ") # {response.text}")
+        return None
+
+
+# Example usage
+email = "fake@email.com"
+password = "password"
+result = authenticate_user(email, password)
+
+print(result)
+print(result.json())
+#print(result.headers)
+
+token = result.json().get('data').get('access_token')
+renewal_token = result.json().get('data').get('renewal_token')
+
+# Example usage
+deviceId = 2
+
+device_data = show_device(deviceId, token)
+
+if device_data:
+    #res = json.loads( device_data.json())
+    #print("data: ", res)
+    #print("data: ", res['content_filters'])
+    print("data: ", device_data.json())
+    print("\n")
+    print("data: ", device_data.json()['content_filters'])
+    print("headers: ",device_data.headers)
+    print("content: ",device_data.content)
+
