675befa610c6a973cfc0dd3265c933f5f34d94cf
_ <thomash1996@live.com>
2024-04-09 15:38:30 -0500

    created a copy all for redis db creation

---
+++ url-list/copy-all-redis.py
@@ -0,0 +1,53 @@
+import redis
+import configparser
+
+def store_lines_in_redis(input_file_path, prefix, redis_host='localhost', redis_port=6379):
+    print("Adding " + input_file_path + " with prefix " + prefix)
+    try:
+        # Connect to the Redis instance
+        r = redis.Redis(host=redis_host, port=redis_port, password=redis_auth, db=redis_db)
+
+        with open(input_file_path, 'r') as input_file:
+            for line in input_file:
+                # Remove leading and trailing whitespaces from the line
+                line = line.strip().lower()
+                
+                # Use the line as the key, and set a dummy value (e.g., "1") in Redis
+                r.set(prefix+":"+line, "1")
+
+        r.connection_pool.disconnect()
+        print("Lines stored in Redis successfully.")
+    except FileNotFoundError:
+        print(f"Input file '{input_file_path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('../server-code/redis-config.ini')
+
+    #redis_host = 'localhost'  # Replace with your Redis server's host
+    #redis_port = 6379  # Replace with your Redis server's port
+    redis_host = config['REDIS']['redis_host']
+    redis_port = int(config['REDIS']['redis_port'])
+    redis_auth = config['REDIS']['redis_auth']
+    redis_db = int(config['REDIS']['redis_db'])
+
+    store_lines_in_redis("nsfw-sites.txt", "nsfw", redis_host, redis_port)
+
+    store_lines_in_redis("lgbt-sites.txt", "lgbt", redis_host, redis_port)
+
+    store_lines_in_redis("genai-sites.txt", "genai", redis_host, redis_port)
+
+    store_lines_in_redis("atheism-sites.txt", "atheism", redis_host, redis_port)
+
+    store_lines_in_redis("nsfw-subs.txt", "nsfw:subreddit", redis_host, redis_port)
+
+    store_lines_in_redis("trans-subs.txt", "trans:subreddit", redis_host, redis_port)
+
+    store_lines_in_redis("lgbt-subs.txt", "lgbt:subreddit", redis_host, redis_port)
+
+    store_lines_in_redis("atheism-subs.txt", "atheism:subreddit", redis_host, redis_port)
+
+
